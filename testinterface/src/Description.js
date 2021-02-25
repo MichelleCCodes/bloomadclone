f8df25e97c99ee556387cca2c5e6f31dce4f5086
Michelle Chen <mchen0062@gmail.com>
2021-02-24 19:25:29 -0800

    added rbg colors

--- testinterface/src/Description.js
+++ testinterface/src/Description.js
@@ -3,13 +3,14 @@ import React from "react";
 const Description = () => {
   return (
     <p>
-      Michelle is an ambitious, curious software developer working closely with
+      <strong>Michelle is an ambitious, curious software developer working closely with
       React. She is excited to contribute her skills in test driven development
       to the team at Bloom. Thank you for sneaking a peek at her work! (Here's
       where you can find... <span></span>
       <a target="_blank" href="https://github.com/MichelleCCodes">
         more
       </a>
+      </strong>
     </p>
   );
 };
