30702403c1142b08649832bdae29d8a1117a567a
Michelle Chen <mchen0062@gmail.com>
2021-02-24 22:29:30 -0800

    completed CSS for clone

--- testinterface/src/App.js
+++ testinterface/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./App.css";
-import Description from "./Description";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-box">
         <div className="App-text">
-          <h1>Be your own therapist</h1>
-          <p>with <strong>cognitive behaviotal therapy</strong></p>
-          <h2>yes, it works.</h2>
+        <h1 className="text-top">Be your own therapist</h1>
+        <p>with <strong>cognitive behaviotal therapy</strong></p>
+        <h1 className="text-bottom">yes, it works.</h1>
         </div>
-        <Description />
-      </header>
+      </div>
+      <p>
+        <strong>
+          Michelle is an ambitious, curious software developer who works closely
+          with React. She is excited to contribute her skills in test driven
+          development to the team at Bloom. Thank you for sneaking a peek at her
+          work! (Here's where you can find... <span></span>
+          <a target="_blank" href="https://github.com/MichelleCCodes">
+            more
+          </a>
+        </strong>
+      </p>
     </div>
   );
 }
