e4cacd925865e7ac81e67d6e65de3ccae6541559
Michelle Chen <mchen0062@gmail.com>
2021-02-24 18:49:11 -0800

    initiated react app

---
+++ testinterface/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import './App.css';
+import Description from './Description'; 
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+      <h1>Be your own therapist</h1>
+      <div> with cognitive</div>
+      <div> behaviotal therapy</div>
+      <h1>yes, it works.</h1>
+      </header>
+    </div>
+  <Description/>
+  </>
+  );
+}
+
+export default App;
---
+++ testinterface/src/Description.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Description = () => { 
+    return (
+        <p>Michelle is an ambitious, curious software developer working closely with React.</p>
+    );
+}
+
+export default Description
